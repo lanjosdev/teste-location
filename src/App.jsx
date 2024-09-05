38b6cf9218488ac536845fc7b7daa14fe5aea3e9
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-09-05 14:22:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude: " + position.coords.latitude);
+      console.log("Longitude: " + position.coords.longitude);
+      setCount(`Latitude: ${position.coords.latitude} / Longitude: ${position.coords.longitude}`);
+    }, function(error) {
+      setCount("Erro ao obter localização: " + error.message);
+    });
+  } else {
+    setCount("Geolocalização não é suportada pelo navegador.");
+  }
+  
+
+  return (
+    <>
+      <div>{count}</div>
+    </>
+  )
+}
+
+export default App
