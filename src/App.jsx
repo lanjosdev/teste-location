825870b4409463e8412cdf9fa1320d56f20f5afa
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-09-05 15:14:45 -0300

    teste onload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
 
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log("Latitude: " + position.coords.latitude);
-      console.log("Longitude: " + position.coords.longitude);
-      setCount(`Latitude: ${position.coords.latitude} / Longitude: ${position.coords.longitude}`);
-    }, function(error) {
-      setCount("Erro ao obter localização: " + error.message);
-    });
-  } else {
-    setCount("Geolocalização não é suportada pelo navegador.");
-  }
+  useEffect(()=> {
+    window.onload = ()=> {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          console.log("Latitude: " + position.coords.latitude);
+          console.log("Longitude: " + position.coords.longitude);
+          setCount(`Latitude: ${position.coords.latitude} / Longitude: ${position.coords.longitude}`);
+        }, function(error) {
+          setCount("Erro ao obter localização: " + error.message);
+        });
+      } else {
+        setCount("Geolocalização não é suportada pelo navegador.");
+      }
+    };
+  }, []);
   
 
   return (
