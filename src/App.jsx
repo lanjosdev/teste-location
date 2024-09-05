ce35e142b919f27e34d2bd6f67379ab494ebaf7e
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-09-05 15:27:54 -0300

    teste 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,37 @@ function App() {
   const [count, setCount] = useState(0);
 
   useEffect(()=> {
-    window.onload = ()=> {
-      if ("geolocation" in navigator) {
+      if("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function(position) {
           console.log("Latitude: " + position.coords.latitude);
           console.log("Longitude: " + position.coords.longitude);
           setCount(`Latitude: ${position.coords.latitude} / Longitude: ${position.coords.longitude}`);
         }, function(error) {
-          setCount("Erro ao obter localização: " + error.message);
+          switch(error.code) {
+            case error.PERMISSION_DENIED:
+              console.log("Permissão negada pelo usuário.");
+              setCount("Permissão negada pelo usuário.");
+              break;
+            case error.POSITION_UNAVAILABLE:
+              console.log("Informação de localização indisponível.");
+              setCount("Informação de localização indisponível.");
+              break;
+            case error.TIMEOUT:
+              console.log("A requisição de localização expirou.");
+              setCount("A requisição de localização expirou.");
+              break;
+            case error.UNKNOWN_ERROR:
+              console.log("Ocorreu um erro desconhecido.");
+              setCount("Ocorreu um erro desconhecido.");
+              break;
+          }
+        }, {
+          timeout: 10000 // Tempo limite de 10 segundos
         });
       } else {
+        console.log("Geolocalização não é suportada pelo navegador.");
         setCount("Geolocalização não é suportada pelo navegador.");
       }
-    };
   }, []);
   
 
